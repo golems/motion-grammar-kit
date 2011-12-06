1a25e39bffe977ee6f547aecd1e7f95a5ac64985
Neil T. Dantam <ntd@gatech.edu>
2011-12-05 20:17:40 -0500

    Refactoring and small fixes
    * DFAP predicate
    
    * DFA-PRUNE: remove dead and unreachable states
    
    * DFA-MINIMIZE-HOPCROFT: prune initially, fix to initial partitions
    
    * a few assertions

--- fa.lisp
+++ fa.lisp
@@ -39,6 +39,12 @@
 ;;;  -(states, tokens, edges, start, accept),
 ;;;    - edges: (list state-0 token state-1)
 
+;;; Representation opptions
+;;; - transition matrix
+;;; - transition hash table
+;;; - edge list
+;;; - successor array
+
 (defstruct (fa (:constructor %make-fa))
   states
   tokens
@@ -106,56 +112,72 @@
               :start (alexandria:ensure-list start)
               :accept (alexandria:ensure-list accept))))
 
-
+;; fixme: multiple start states
 (defun make-fa-renumber (edges start accept)
   (let ((state-hash (make-hash-table :test #'equal))
         (token-hash (make-hash-table :test #'equal))
         (state-counter -1)
         (token-counter 0)
         nedges)
-    (setf (gethash :epsilon token-hash) 0)
-    ;; start/accept states
-    (map nil (lambda (x)
-               (setf (gethash x state-hash)
-                     (incf state-counter)))
-         (alexandria:ensure-list start))
-    (map nil (lambda (x)
-               (setf (gethash x state-hash)
-                     (incf state-counter)))
-         (alexandria:ensure-list accept))
-    ;; build hashes
-    (dolist (e edges)
-      (destructuring-bind (q0 z q1) e
-        (unless (gethash q0 state-hash)
-          (setf (gethash q0 state-hash) (incf state-counter)))
-        (unless (gethash q1 state-hash)
-          (setf (gethash q1 state-hash) (incf state-counter)))
-        (unless (gethash z token-hash)
-          (setf (gethash z token-hash) (incf token-counter)))
-        (push (list (gethash q0 state-hash)
-                    (gethash z token-hash)
-                    (gethash q1 state-hash))
-              nedges)))
-    ;; map hashes to build new edges and mapping arrays
-    (let ((state-array (make-array (1+ state-counter)))   ; i -> q
-          (token-array (make-array (1+ token-counter))))  ; i -> z
-      (maphash (lambda (n d) (setf (aref state-array d) n))
-               state-hash)
-      (maphash (lambda (n d) (setf (aref token-array d) n))
-               token-hash)
-      (%make-fa :states state-array
-                :tokens token-array
-                :edges nedges
-                :start (mapcar (lambda (x) (gethash x state-hash))
-                               (alexandria:ensure-list start))
-                :accept (mapcar (lambda (x) (gethash x state-hash))
-                               (alexandria:ensure-list accept))))))
+    (labels ((new-state (name)
+               (unless (gethash name state-hash)
+                 (setf (gethash name state-hash)
+                       (incf state-counter)))
+               (gethash name state-hash)))
+      (setf (gethash :epsilon token-hash) 0)
+      ;; start/accept states
+      (map nil #'new-state (alexandria:ensure-list start))
+      (map nil #'new-state (alexandria:ensure-list accept))
+      ;; build hashes
+      (dolist (e edges)
+        (destructuring-bind (q0 z q1) e
+          (unless (gethash z token-hash)
+            (setf (gethash z token-hash) (incf token-counter)))
+          (push (list (new-state q0) (gethash z token-hash) (new-state q1))
+                nedges)))
+      ;; map hashes to build new edges and mapping arrays
+      (let ((state-array (make-array (1+ state-counter)
+                                     :initial-element nil))   ; i -> q
+            (token-array (make-array (1+ token-counter)
+                                     :initial-element nil)))  ;  i -> z
+        (maphash (lambda (n d)
+                   ;;(format t "~&[~A]: ~A~&" d n)
+                   (setf (aref state-array d) n))
+                 state-hash)
+        (maphash (lambda (n d) (setf (aref token-array d) n))
+                 token-hash)
+        (%make-fa :states state-array
+                  :tokens token-array
+                  :edges nedges
+                  :start (mapcar #'new-state (alexandria:ensure-list start))
+                  :accept (mapcar #'new-state
+                                  (alexandria:ensure-list accept)))))))
 
 
-(defun make-fa (edges start accept &key (renumber-symbols t))
-  (if renumber-symbols
-      (make-fa-renumber edges start accept)
-      (make-fa-simple edges start accept)))
+(defun make-fa (edges start accept &key (renumber-symbols t) (dfa-sort nil))
+  (let ((fa (if renumber-symbols
+                (make-fa-renumber edges start accept)
+                (make-fa-simple edges start accept))))
+    (if dfa-sort
+        (dfa-sort fa)
+        fa)))
+
+
+(defun dfap (fa)
+  "True if FA is deterministic"
+  (let ((mover (fa-mover fa)))
+    (when (> (length (fa-start fa)) 1)
+      (return-from dfap (values nil -1 0)))
+    (dotimes (i (length (fa-states fa)))
+      ;; epsilon
+      (when (funcall mover i 0)
+        (return-from dfap (values nil i 0)))
+      ;; others
+      (loop for j from 1 below (length (fa-tokens fa))
+         do (when (> (length (funcall mover i j)) 1)
+              (return-from dfap (values nil i j)))))
+    fa))
+
 
 (defun dfa-sort (fa &key preserve-states)
   "Sort all elements of the FA into canonical (ascending) order.
@@ -205,28 +227,13 @@ PRESERVE-STATES: If true, sort state names.
                            (visit (car (funcall mover i-q (aref token-old i-z))))))))
               (visit (car (fa-start fa)))))))
     ;; Build sorted FA
-    (%make-fa :states (if preserve-states
-                          (map 'vector (lambda (i) (aref (fa-states fa) i)) state-indices)
-                          state-indices)
-              :tokens (map 'vector (lambda (i) (aref (fa-tokens fa) i)) token-indices)
-              :start (sort (map 'list
-                                (lambda (i) (aref state-old i))
-                                (fa-start fa))
-                           #'<)
-              :accept (sort (map 'list
-                                 (lambda (i) (aref state-old i))
-                                 (fa-accept fa))
-                            #'<)
-              :edges (sort (map 'list (lambda (e)
-                                        (list (aref state-old (first e))
-                                              (aref token-old (second e))
-                                              (aref state-old (third e))))
-                                (fa-edges fa))
-                           #'symbol-list-compare))))
+    (fa-renumber fa :state-map state-old :token-map token-old)))
 
 
 (defun dfa-equal (a b)
   "Check equivalence up to state names of DFAs"
+  (assert (dfap a))
+  (assert (dfap b))
   (equalp (dfa-sort a) (dfa-sort b)))
 
 (defun fa-mover (fa)
@@ -241,7 +248,20 @@ PRESERVE-STATES: If true, sort state names.
                           :initial-element nil)))
     (loop for (q0 z q1) in (fa-edges fa)
        do (push q0 (aref move q1 z)))
+    ;;(format t "~&~A" move)
     (lambda (i-state i-token) (aref move i-state i-token))))
+
+(defun fa-successor-array (fa)
+  "Make array indexed by original state of edge lists from that state."
+  (let ((array (make-array (length (fa-states fa)) :initial-element nil)))
+    (map nil (lambda (e) (push e (aref array (car e)))) (fa-edges fa))
+    array))
+
+(defun fa-predecessor-array (fa)
+  "Make array indexed by final state of edge lists to that state."
+  (let ((array (make-array (length (fa-states fa)) :initial-element nil)))
+    (map nil (lambda (e) (push e (aref array (caddr e)))) (fa-edges fa))
+    array))
 
 (defun fa-dot (fa &optional output )
   "Graphviz output of dfa.
@@ -259,16 +279,23 @@ output: output file, type determined by suffix (png,pdf,eps)"
                name))
            (helper (s)
              (format s "~&digraph {~%")
+             ;; state labels
+             (format s "~:{~&  ~A[label=\"~A\"];~}"
+                     (loop for i below (length (fa-states fa))
+                          collect (list i (state-label i))))
+             ;; start state
              (when (fa-start fa)
                (format s "~&  start[shape=none];")
                (format s "~{~&  start -> ~A;~}"
-                       (map 'list #'state-label (alexandria:ensure-list (fa-start fa)))))
+                       (map 'list #'identity
+                            (alexandria:ensure-list (fa-start fa)))))
+             ;; accept state
              (format s "~{~&  ~A [ shape=doublecircle ];~}"
-                     (map 'list #'state-label (alexandria:ensure-list (fa-accept fa))))
+                     (map 'list #'identity
+                          (alexandria:ensure-list (fa-accept fa))))
              (map 'nil (lambda (x)
                          (format s "~&  ~A -> ~A [label=\"~A\"];~%"
-                                 (state-label (first x))
-                                 (state-label (third x) )
+                                 (first x) (third x)
                                  (token-label (second x))))
                   (fa-edges fa))
              (format s "~&}~%"))
@@ -300,6 +327,91 @@ output: output file, type determined by suffix (png,pdf,eps)"
             :edges (mapcar #'reverse (fa-edges fa))
             :start (fa-accept fa)
             :accept (fa-start fa)))
+
+
+(defun fa-prune (fa)
+  "Remove unreachable and dead states from the FA."
+  (let ((pred (fa-predecessor-array fa))
+        (succ (fa-successor-array fa))
+        (live-rev (make-array (length (fa-states fa)) :element-type 'bit))
+        (live-fwd (make-array (length (fa-states fa)) :element-type 'bit))
+        (state-map (make-array (length (fa-states fa)) :initial-element nil)))
+    ;; mark forward live states
+    (labels ((visit (i)
+               (when (zerop (aref live-fwd i))
+                 (setf (aref live-fwd i) 1)
+                 (map nil (lambda (e) (visit (third e))) (aref succ i)))))
+      (map nil #'visit (fa-start fa)))
+    ;; mark reverse live states
+    (labels ((visit (i)
+               (when (zerop (aref live-rev i))
+                 (setf (aref live-rev i) 1)
+                 (map nil (lambda (e) (visit (car e))) (aref pred i)))))
+      (map nil #'visit (fa-accept fa)))
+    ;; find reorder mapping
+    (loop
+       for i from 0 ; original index
+       with j = -1   ; new index
+       for x across live-fwd
+       for y across live-rev
+       unless (or (zerop x) (zerop y))
+       do (setf (aref state-map i)
+                (incf j)))
+    (fa-renumber fa :state-map state-map)))
+
+
+(defun fa-renumber (fa &key state-map token-map)
+  "Reorder states and tokens in the FA.
+STATE-MAP: (aref state-map old-index) => (or new-index nil)
+TOKEN-MAP: (aref token-map old-index) => (or new-index nil)"
+  (labels ((new-state (i)
+             (if state-map
+                 (aref state-map i)
+                 i))
+           (new-token (i)
+             (if token-map
+                 (aref token-map i)
+                 i)))
+    (%make-fa :states  (cond ((null state-map) (fa-states fa))
+                             (t (map 'vector #'identity
+                                     (loop
+                                        with i = 0
+                                        for x across state-map
+                                        when x
+                                        collect i
+                                        do (incf i)))))
+              :tokens (if (null token-map)
+                          (fa-tokens fa)
+                          (let ((tokens (make-array (loop
+                                                       for x across token-map
+                                                       count x)
+                                                    :initial-element nil)))
+                            (loop with i = 0
+                               for x across token-map
+                               when x
+                               do (setf (aref tokens (aref token-map i))
+                                        (aref (fa-tokens fa) i)))
+                            tokens))
+              :start (sort (mapcan
+                          (lambda (i) (when (new-state i)
+                                   (list (new-state i))))
+                          (fa-start fa))
+                         #'<)
+              :accept (sort (mapcan
+                             (lambda (i) (when (new-state i)
+                                      (list (new-state i))))
+                             (fa-accept fa))
+                            #'<)
+              :edges (sort (mapcan
+                            (lambda (e)
+                              (when (and (new-state (first e))
+                                         (new-token (second e))
+                                         (new-state (third e)))
+                                (list (list (new-state (first e))
+                                            (new-token (second e))
+                                            (new-state (third e))))))
+                                (fa-edges fa))
+                           #'symbol-list-compare))))
 
 
 ;;; Regex
@@ -412,15 +524,25 @@ MOVER: fuction from (state-0 token) => (list state-1-0 state-1-1...)"
                  for u = (sort (nfa-move-e-closure ds i-z mover) #'<)
                  when u
                  do (progn
+                      (assert (null (set-difference u (nfa-e-closure u mover))))
                       (when (not (gethash u d-hash))
                         (setf (gethash u d-hash) (length d-states))
                         (vector-push-extend u d-states))
                       (push (list mark (fa-token-name nfa i-z) (gethash u d-hash))
                             d-edge)))))
       ;; result
-      (make-fa d-edge
-               (fa-subset-indices (fa-start nfa) d-states)
-               (fa-subset-indices (fa-accept nfa) d-states)))))
+      ;;(print nfa)
+      ;;(print d-states)
+      ;;(print (fa-subset-indices (fa-start nfa) d-states))
+      ;;(print (fa-subset-indices (fa-accept nfa) d-states))
+      (loop for ds across d-states
+         do (assert (= 1 (loop for dt across d-states
+                            counting (equal ds dt)))))
+      (let ((fa (make-fa d-edge
+                         0
+                         (fa-subset-indices (fa-accept nfa) d-states))))
+        (assert (dfap fa))
+        fa))))
 
 ;; Brzozowski's Algorithm
 (defun dfa-minimize-brzozowski (dfa)
@@ -428,9 +550,11 @@ MOVER: fuction from (state-0 token) => (list state-1-0 state-1-1...)"
 
 ;; Hopcroft's Algorithm
 (defun dfa-minimize-hopcroft (dfa)
-  (let ((p (list (fa-accept dfa)
-                 (set-difference (map 'list #'identity (fa-states dfa))
-                                 (fa-accept dfa)))))
+  (let* ((dfa (fa-prune dfa)) ; pruning first should make things faster
+         (p (list (fa-accept dfa)
+                  (set-difference (loop for i below (length (fa-states dfa))
+                                     collect i)
+                                  (fa-accept dfa)))))
     ;; build minimal states
     (loop
        with imover = (fa-inv-mover dfa)
@@ -471,9 +595,12 @@ MOVER: fuction from (state-0 token) => (list state-1-0 state-1-1...)"
                               (position-if (lambda (q) (find (car q-next) q))
                                            p))
                         edges)))
-      (make-fa edges
-               (fa-subset-indices (fa-start dfa) p)
-               (fa-subset-indices (fa-accept dfa) p)))))
+
+      (let ((fa (make-fa edges
+                         (fa-subset-indices (fa-start dfa) p)
+                         (fa-subset-indices (fa-accept dfa) p))))
+        (assert (dfap fa))
+        fa))))
 
 
 ;; (defun nfa-e-close (n e-lookup)
