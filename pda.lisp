dafa6eb253d35ac9e9f73c5a60f6125231c657d1
Neil T. Dantam <ntd@gatech.edu>
2012-05-10 23:35:14 -0400

    PDA construction according to Sipser

--- pda.lisp
+++ pda.lisp
@@ -45,6 +45,15 @@
   accept)
 
 
+(defun pda-edge-extended (q-0 z g-0 q-1 g-1-list)
+  "Make a PDA edge."
+  (list (list q-0 z g-0)
+        (cons q-1 g-1-list)))
+
+(defun pda-edge (q-0 z g-0 q-1 &rest g-1)
+  "Make a PDA edge."
+  (pda-edge-extended q-0 z g-0 q-1 g-1))
+
 (defun pda-map-transtions (result-type function pda)
   "Apply FUNCTION to all transitions of PDA.
 FUNCTION: (lambda ( (list q-0 z g-0) (list q-1 g-1))"
@@ -53,7 +62,7 @@ FUNCTION: (lambda ( (list q-0 z g-0) (list q-1 g-1))"
     (do-finite-set (sigma (pda-input-alphabet pda))
       (do-finite-set (gamma (pda-stack-alphabet pda))
         (do-finite-set (edge (funcall (pda-transition pda) q sigma gamma))
-          (format t "edge: ~A => ~A~&" (list q sigma gamma) edge)
+          ;(format t "edge: ~A => ~A~&" (list q sigma gamma) edge)
           (funcall function (list q sigma gamma) edge ))))))
 
 
@@ -63,13 +72,17 @@ FUNCTION: (lambda ( (list q-0 z g-0) (list q-1 g-1))"
         (input-alphabet (make-finite-set))
         (stack-alphabet (make-finite-set)))
     (loop
-       for (x y) in edges
-       for ((q-0 sigma gamma-0) (q-1 gamma-1)) in edges
+       for edge in edges
        do
-         (setf (gethash x hash) (finite-set-add (gethash x hash) y)
-               states (finite-set-union states (finite-set q-0 q-1))
-               input-alphabet (finite-set-add input-alphabet sigma)
-               stack-alphabet (finite-set-union stack-alphabet (finite-set gamma-0 gamma-1))))
+         (destructuring-bind (x y) edge
+           (destructuring-bind ((q-0 sigma gamma-0) (q-1 &rest gamma-1)) edge
+             ;(print edge)
+             (setf (gethash x hash) (finite-set-add (gethash x hash) y)
+                   states (finite-set-union states (finite-set q-0 q-1))
+                   input-alphabet (finite-set-add input-alphabet sigma)
+                   stack-alphabet (finite-set-union stack-alphabet
+                                                    (fold #'finite-set-add (make-finite-set)
+                                                          (cons gamma-0 gamma-1)))))))
     (assert (finite-set-inp start states))
     (assert (finite-set-subsetp accept states))
     (%make-pda :states states
@@ -90,7 +103,6 @@ FUNCTION: (lambda ( (list q-0 z g-0) (list q-1 g-1))"
                           (finite-set-map 'list (lambda (state)
                                                   (list (funcall state-numbers state) state font-size))
                                           (pda-states pda)))
-
                   ;; start state
                   (when (pda-start pda)
                     (format s "~&  start[shape=none,fontsize=~D];" font-size)
@@ -102,11 +114,37 @@ FUNCTION: (lambda ( (list q-0 z g-0) (list q-1 g-1))"
                   ;; transitions
                   (pda-map-transtions nil
                                       (lambda (x y)
-                                        (destructuring-bind ((q-0 z g-0) (q-1 g-1)) (list x y)
-                                          (format s "~&  ~A -> ~A [fontsize=~D,label=\"~A,~A&rarr;~A\"];~%"
+                                        (destructuring-bind ((q-0 z g-0) (q-1 &rest g-1)) (list x y)
+                                          (format s "~&  ~A -> ~A [fontsize=~D,label=\"~A,~A&rarr;~{~A~}\"];~%"
                                                   (funcall state-numbers q-0)
                                                   (funcall state-numbers q-1)
                                                   font-size
-                                                  (dot-gsymbol z) (dot-gsymbol g-0) (dot-gsymbol g-1))))
+                                                  (dot-gsymbol z) (dot-gsymbol g-0)
+                                                  (map 'list #'dot-gsymbol g-1))))
                                       pda)
                   (format s "~&}~%")))))
+
+;; Sipser p116
+(defun grammar->pda-sipser (grammar &key (gensym (gensym "pda")))
+  (let ((counter -1))
+    (labels ((make-state (&optional (val (incf counter)))
+               (cons gensym val)))
+      (let* ((q-start (make-state :start))
+             (q-accept (make-state :accept))
+             (q-loop (make-state :loop))
+             (edges (list (pda-edge q-start :epsilon :epsilon q-loop (grammar-start-nonterminal grammar) :$)
+                          (pda-edge q-loop :epsilon :$ q-accept :epsilon))))
+        ;; production edges
+        (grammar-map nil
+                     (lambda (head body)
+                       (push (pda-edge-extended q-loop :epsilon head q-loop body)
+                             edges))
+                     grammar)
+        ;; token edges
+        (finite-set-map nil
+                        (lambda (z)
+                          (push (pda-edge q-loop z z q-loop :epsilon)
+                                edges))
+                        (grammar-terminals grammar))
+        ;; construct
+        (make-pda edges q-start (finite-set q-accept))))))
